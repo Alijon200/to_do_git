2c15e9b7d9e479321f8557c87f67b8147b7895a1
Alijon <you@example.com>
2025-08-27 15:55:31 +0500

    4-bosqich

--- servece.py
+++ servece.py
@@ -1,7 +1,17 @@
 
-tasks = {}
 def add_todo():
-    task_id = 1
+    try:
+        with open("malumot.txt", "r") as file:
+            lines = [line.strip() for line in file.readlines() if line.strip()]
+
+            if lines:
+                last_line = lines[-1]
+                last_id = int(last_line.split(".")[0])
+                task_id = last_id + 1
+            else:
+                task_id = 1
+    except FileNotFoundError:
+        task_id = 1
 
     while True:
         task = input("Vazifa kiriting (chiqish uchun 'stop'): ")
@@ -10,30 +20,55 @@ def add_todo():
             break
 
         if task:
-            tasks[task_id] = task
-            print(f"Vazifa qo'shildi!")
+            with open("malumot.txt", "a") as file:
+                file.write(f"{task_id}. {task}\n")
+            print(f"Vazifa qo'shildi! ID: {task_id}")
             task_id += 1
         else:
             print("Iltimos vazifa kiriting!")
 
-    return tasks
+
 
 
 def view_tasks():
-    if tasks:
-        print("====== Sizning vazifalaringiz =======")
-        for i, task in tasks.items():
-            print(f"{i}. {task} ")
-    else:
-        print("Hozircha sizda vazida mavjud emas!")
+    try:
+        with open("malumot.txt", "r") as file:
+            content = file.read().strip()
+            if content:
+                print("====== Sizning vazifalaringiz =======")
+                print(content)
+            else:
+                print("Hozircha sizda vazifa mavjud emas!")
+    except FileNotFoundError:
+        print("Hali 'malumot.txt' fayli yaratilmagan!")
 
 
 def delete_task():
     view_tasks()
-    number = int(input("O'chirmoqchi bolgan taskni raqamini kiriting: "))
-    if number in tasks.keys():
-        del  tasks[number]
-        print("=== Vazifa o'chirildi ===")
+    task_id = input("Qaysi vazifani o‘chirmoqchisiz (raqam kiriting): ")
 
-    else:
-        print("Ushbu nomerli vazifa topilmadi!")
+    try:
+        with open("malumot.txt", "r") as file:
+            lines = file.readlines()
+
+        found = False
+        new_lines = []
+
+        for line in lines:
+            if not line.startswith(f"{task_id}."):
+                new_lines.append(line)
+            else:
+                found = True
+
+        with open("malumot.txt", "w") as file:
+            file.writelines(new_lines)
+
+        if found:
+            print(f"{task_id}-raqamli vazifa o‘chirildi!")
+        else:
+            print("Bunday raqamli vazifa topilmadi.")
+
+    except FileNotFoundError:
+        print("Hali 'malumot.txt' fayli yaratilmagan!")
+
+
